2a69d766436d56c7f747e8df0335911ece40b8e8
Aliaksei_Akulau <akulaualiaksei@gmail.com>
2020-10-31 13:48:18 +0300

    feat: simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,52 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  result : [],
   getLength() {
-    throw new CustomError('Not implemented');
+    // return
+    this.result.length;
+    return this;
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
+    // return
+    this.result.push(value);
+    return this;
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
+    if (Number.isInteger(position)) {
+      // return
+      this.result.splice(position - 1 , 1);
+      return this;
+    } else {
+      this.result = [];
+      throw new Error('wrong number in remove position');
+    }
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
+    // throw new CustomError('Not implemented');
+    // return
+    this.result.reverse();
+    return this;
     // remove line with error and write your code here
   },
   finishChain() {
-    throw new CustomError('Not implemented');
+    // throw new CustomError('Not implemented');
+    let stringResult = '';
+    for (i = 0; i < this.result.length; i++) {
+      if (stringResult.length === 0) {
+        stringResult = `${stringResult}( ${this.result[i]} )`;
+      } else {
+        stringResult = `${stringResult}~~( ${this.result[i]} )`;
+      }
+    }
+    this.result = [];
+    return stringResult;
     // remove line with error and write your code here
   }
 };
